2ba3991d1777236725a8f9824e665fda368d0b0a
Kelvin Ashong <kksashong@st.ug.edu.gh>
2024-07-11 14:07:38 +0000

    created the header part of my home screen

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,SafeAreaView,  TouchableOpacity} from 'react-native';
+import CartScreen from './CartScreen';
+
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>HomeScreen</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={{ flex: 1 }}>
+    
+    <View style={styles.header}>
+    <TouchableOpacity >
+    <Image source={require('./assets/Menu.png')} style={{ width: 24, height: 24 }} />
+    </TouchableOpacity>
+    <Image source={require('./assets/Logo.png')} style={{ width: 230, height: 90 ,alignSelf: 'center' }} />
+    
+    <View style={{flexDirection:"row"}}>
+       <TouchableOpacity>
+     <Image source={require('./assets/Search.png')} style={{ width: 24, height: 24, marginRight: 10 }} />
+     </TouchableOpacity>
+     <TouchableOpacity>
+     <Image source={require('./assets/shopping bag.png')} style={{ width: 24, height: 24 }} />
+       </TouchableOpacity>
+     </View>
     </View>
+
+    
+    <View style={styles.subheader}>
+                <Text style={{ fontSize: 24, padding: 10 }}>OUR STORY</Text>
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity>
+                        <Image source={require('./assets/Listview.png')} style={{ width: 30, height: 30, marginRight: 10 }} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={require('./assets/Filter.png')} style={{ width: 40, height: 24 }} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+            
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  header:{
+    flexDirection: "row",
+    columnGap:10,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    marginTop: 40
   },
+  subheader:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center', 
+    padding: 10 
+  }
 });
